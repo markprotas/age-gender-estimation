72a4e176e4500c79032ffb7a5b4c43ce3c2f09b3
Mark Protas <mark.protas@accenture.com>
2019-03-14 13:18:02 -0400

    Added images for remaining classes

--- ads/ad_classes.py
+++ ads/ad_classes.py
@@ -9,3 +9,7 @@ class AdClasses(Enum):
     def get_image_path(self):
         if self == AdClasses.GENERIC:
             return "soccer_ball.jpg"
+        elif self == AdClasses.MIDDLE_AGED_MEN:
+            return "beers.jpg"
+        elif self == AdClasses.FAMILIES:
+            return "games.jpg"
