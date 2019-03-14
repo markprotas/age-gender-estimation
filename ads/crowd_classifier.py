f74a54a850b134645a4375849c74b6ef6c6a202a
Mark Protas <mark.protas@accenture.com>
2019-03-14 11:47:29 -0400

    Added families class to classifier

--- ads/crowd_classifier.py
+++ ads/crowd_classifier.py
@@ -4,5 +4,7 @@ from ads.ad_classes import AdClasses
 class CrowdClassifier(object):
     def classify(self, gender_age_tuples):
         if len(gender_age_tuples) > 1:
-            return AdClasses.MIDDLE_AGED_MEN
+            if all(ga.gender == 'M' for ga in gender_age_tuples):
+                return AdClasses.MIDDLE_AGED_MEN
+            return AdClasses.FAMILIES
         return AdClasses.GENERIC
--- tests/test_crowd_classifier.py
+++ tests/test_crowd_classifier.py
@@ -17,3 +17,7 @@ def test_should_resolve_generic_ad_class_for_empty_list(crowd_classifier):
 def test_should_resolve_middle_aged_men_if_all_faces_identified_are_men_between_30_and_55(crowd_classifier):
     faces = [IdentifiedFace('M', 35), IdentifiedFace('M', 43), IdentifiedFace('M', 50)]
     assert crowd_classifier.classify(faces) == AdClasses.MIDDLE_AGED_MEN
+
+def test_should_resolve_families_if_mixed_genders_and_ages(crowd_classifier):
+    faces = [IdentifiedFace('M', 42), IdentifiedFace('F', 38), IdentifiedFace('M', 7)]
+    assert crowd_classifier.classify(faces) == AdClasses.FAMILIES
